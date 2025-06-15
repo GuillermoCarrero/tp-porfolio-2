ed6743280bb65ee0b85764669df38a5f53ac7428
GuillermoCarrero <www.gcarrerollovera.world@gmail.com>
2025-06-14 21:01:38 -0300

    second commit: completando pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,14 @@
 
-import { useState } from "react";
-import { LoadingScreen } from "./components/LoadingScreen";
-import Header from "./components/Header";
-import { Home } from "./components/sections/Home";
-import { About } from "./components/sections/About";
-import { Projects } from "./components/sections/Projects";
-import { Contact } from "./components/sections/Contacts";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 import "./index.css";
-import Footer from "./components/Footer";
-import useThemeStore from './store/themeStore'; // Importa el store
+import MainHome from "./pages/MainHome"
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const Alumno = {
-    redes: {
-      instagram: "https://www.instagram.com/guillermojosecarrero/",
-      linkedin: "https://www.linkedin.com/in/guillermo-jose-carrero-llovera-80b994202/",
-      github: "https://github.com/GuillermoCarrero",
-    },
-  };
 
-  const { darkMode } = useThemeStore(); // Obtiene el estado darkMode del store
 
   return (
     <>
-    <BrowserRouter>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-      <div
-        className={`min-h-screen transition-colors duration-700 ${
-          isLoaded ? "opacity-100" : "opacity-0"
-        } ${darkMode ? "bg-gray-900 text-gray-100" : "bg-white text-gray-900"}`} // Clases condicionales
-      >
-        <Header/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer datos={Alumno} />
-      </div>
-    </BrowserRouter>
+    <MainHome/>
     </>
   );
 }
--- src/pages/MainHome.jsx
+++ src/pages/MainHome.jsx
@@ -1,13 +1,16 @@
-
 import { useState } from "react";
-import { LoadingScreen } from "./components/LoadingScreen";
+import { LoadingScreen } from "../components/LoadingScreen";
 import Header from "../components/Header";
+import { Home } from "../components/sections/Home";
+import { About } from "../components/sections/About";
+import { Projects } from "../components/sections/Projects";
+import { Contact } from "../components/sections/Contacts";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Footer from "../components/Footer";
+import useThemeStore from '../store/themeStore'; 
 
-const MainHome = () => {
-
+export const MainHome = () => {
   const [isLoaded, setIsLoaded] = useState(false);
-
   const Alumno = {
     redes: {
       instagram: "https://www.instagram.com/guillermojosecarrero/",
@@ -16,26 +19,29 @@ const MainHome = () => {
     },
   };
 
-  return (
+  const { darkMode } = useThemeStore(); 
 
-    <div>
+  return (
+    <>
+    <BrowserRouter>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
       <div
-        className={`min-h-screen transition-opacity duration-700 ${
+        className={`min-h-screen transition-colors duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
-        } bg-black text-gray-100`}
+        } ${darkMode ? "bg-gray-900 text-gray-100" : "bg-white text-gray-900"}`} 
       >
         <Header/>
-        <MainHome/>
-        <Footer datos={Alumno}/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer datos={Alumno} />
       </div>
-    </div>
+    </BrowserRouter>
+    </>
   );
-};
+}
 
 export default MainHome;
-
-
-
-
-  
